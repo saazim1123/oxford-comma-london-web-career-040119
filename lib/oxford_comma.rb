1a24db181c3f5c79ae6501edc6fc71b4f5da0a0f
Adnan Azim <saazim1123@gmail.com>
2019-03-11 03:27:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,17 @@
 def oxford_comma(array)
- array.join(" ")
- 
+ if array.length < 2 
+   array.join(" ")
+   
+ elsif array.length == 2 
+  array.join(" and ")
+
+ elsif array.length == 3
+  last_element = array.pop
+  array.join(", ") + (", and ") + last_element
+  
+  else array.length >= 3
+    last_element = array.pop
+    array.join(", ") + (", and ") + last_element
+  end
 end
+
